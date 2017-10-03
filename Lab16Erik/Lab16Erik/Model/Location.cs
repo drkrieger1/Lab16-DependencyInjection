95ab99db95d9e5689ada0c7bd49280fbb875b80a
drkrieger <eplyushko@gmail.com>
2017-10-03 10:12:13 -0700

    made controllrs,model,view

---
+++ Lab16Erik/Lab16Erik/Model/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16Erik.Model
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longited { get; set; }
+        public string WeatherType { get; set; }
+        public double AvTemp { get; set; }
+    }
+}
