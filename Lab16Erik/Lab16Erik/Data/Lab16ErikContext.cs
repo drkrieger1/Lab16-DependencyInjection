2b1ae2692e52dbb916b47d0179aba5bdc6189343
drkrieger <eplyushko@gmail.com>
2017-10-03 10:23:12 -0700

    scafold/lacations controller added

---
+++ Lab16Erik/Lab16Erik/Data/Lab16ErikContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lab16Erik.Model;
+
+namespace Lab16Erik.Models
+{
+    public class Lab16ErikContext : DbContext
+    {
+        public Lab16ErikContext (DbContextOptions<Lab16ErikContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab16Erik.Model.Location> Location { get; set; }
+    }
+}
